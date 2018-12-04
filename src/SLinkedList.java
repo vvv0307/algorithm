3797106b12ed27b6dbf9946a01089925013d036d
Zhang HaoTian(智能媒体研发中心) <haotianzhang215720@sohu-inc.com>
2018-12-04 20:02:27 +0800

    add constructor use array init linklist

--- src/SLinkedList.java
+++ src/SLinkedList.java
@@ -7,38 +7,32 @@ public class SLinkedList {
     private int length;
     private SNode head;
     private SNode last;
-    public SNode getHead(){
-        return head;
-    }
     public int length(){
         return length;
     }
     public SLinkedList(int[] array){
         this.length = array.length;
-        for(int i = 0;i<length;i++){
-            SNode newNode = new SNode(array[i]);
-            if(length==0){
-                head.next = last;
-            }
-            last = newNode;
-            last = last.next;
-            length = length + 1;
+        SNode first = new SNode(array[0]);
+        this.head = new SNode();
+        head.next = first;
+        for(int i = 1;i<array.length;i++){
+            SNode sNode = new SNode(array[i]);
+            first.next = sNode;
+            first = first.next;
         }
-
+        last = first;
     }
     public void addNode(int newValue){
-        SNode newNode = new SNode(newValue);
-        last = last.next;
-        last = newNode;
-        length = length + 1;
+
     }
     public static void main(String[] args){
         int[] array = {0,1,3,4,5};
         SLinkedList sLinkedList = new SLinkedList(array);
-        SNode head = sLinkedList.getHead();
-        head = head.next;
-        if(head.next!=null){
-            System.out.println(head.value);
+        //System.out.println(sLinkedList.last.value);
+        SNode sNode =sLinkedList.head.next;
+        while (sNode != null){
+            System.out.println(sNode.value);
+            sNode = sNode.next;
         }
     }
 }
@@ -49,6 +43,9 @@ public class SLinkedList {
 class SNode{
     SNode next;
     int value;
+    public SNode(){}
+
+
     public SNode(int value){
         this.value = value;
     }
